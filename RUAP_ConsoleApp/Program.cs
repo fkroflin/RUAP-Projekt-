0521c2e2e16a24ad34df596d99ce33a432872a96
Fran <fran.kroflin@gmail.com>
2024-11-05 12:02:45 +0100

    Zadatak 3

--- RUAP_ConsoleApp/Program.cs
+++ RUAP_ConsoleApp/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
+<<<<<<< HEAD
             Console.WriteLine("Pozdrav, iz aplikacije od Frana Kroflina! INDEX");
+=======
+            Console.WriteLine("Pozdrav, iz aplikacije od Frana Kroflina!");
+>>>>>>> f771e0b (Rijesio 7 zadatak)
         }
     }
 }
